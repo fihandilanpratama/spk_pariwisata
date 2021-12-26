e32f983c25445fc44c6291a622961c3e331f04dc
fihanDilanPratama <fihandilan96@gmail.com>
2021-12-26 18:01:59 +0900

    ready 3.3

--- spk_pariwisata/settings.py
+++ spk_pariwisata/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku #
-# import dj_database_url #
-# from decouple import config #
+import dj_database_url #
+from decouple import config #
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
