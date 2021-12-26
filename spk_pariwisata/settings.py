17cd46e0eac70d8f0e31372b1cf8a82aa3fedc2f
fihanDilanPratama <fihandilan96@gmail.com>
2021-12-26 17:33:35 +0900

    update STATIC_ROOT

--- spk_pariwisata/settings.py
+++ spk_pariwisata/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # static directory
+MEDIA_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
